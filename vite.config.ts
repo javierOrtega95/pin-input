0be83e1d5f6d6343efd863d46f01414427375bf5
javierOrtega95 <fjortega@wizzie.io>
2026-04-16 12:43:08 +0200

    chore: configure package.json and vite.config.ts for npm publication

--- vite.config.ts
+++ vite.config.ts
@@ -6,12 +6,9 @@ export default defineConfig({
   build: {
     lib: {
       entry: resolve(__dirname, 'src/pin-input.ts'),
-
       name: 'PinInput',
-
       fileName: 'pin-input',
-
-      formats: ['es', 'cjs', 'umd'],
+      formats: ['es', 'umd'],
     },
   },
   test: {
